6d909bda24c91e693591e6363f6ebde9824fb5dd
Vedant <156441728+itsVedantSharma@users.noreply.github.com>
2024-06-15 16:38:15 +0530

    Add files via upload

---
+++ Pattern5.cpp
@@ -0,0 +1,21 @@
+// 1
+// 2 3
+// 4 5 6 for n=3
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    int count=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<count<<" ";
+            j++;
+            count++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
