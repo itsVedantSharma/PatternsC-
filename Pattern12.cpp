e2218fa518e94b973e07b193e4b0f810185099b1
Vedant <156441728+itsVedantSharma@users.noreply.github.com>
2024-06-18 23:54:20 +0530

    Add files via upload

---
+++ Pattern12.cpp
@@ -0,0 +1,16 @@
+//     *
+//   * *
+// * * * for n=3
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            
+        }
+    }
+}
